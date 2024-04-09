9dade5ec2375e94270ba0a3286b15aef2395479c
Jacob Cortez <jacob.cortez@teamseco.com>
2024-04-09 17:37:23 -0400

    Simple username & password check when button clicked

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
-import java.awt.*;
-import java.util.ResourceBundle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class Main extends JFrame{
     private JPanel LoginPanel;
     private JLabel ACME;
-    private JTextField Username;
+    private  JTextField Username;
     private JPasswordField Password;
+    private JButton LoginButton;
 
 
     public Main (){
@@ -18,13 +20,22 @@ public class Main extends JFrame{
         setLocationRelativeTo(null);
         setVisible(true);
         ACME.setText("ACME INC");
-        
+        LoginButton.setText("LOGIN");
+        LoginButton.addActionListener(new ActionListener() {
 
-
-
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkLogin();
+            }
+    });
     }
-
+public void checkLogin(){
+    if(Objects.equals(Username.getText(), "Test") && Objects.equals(Password.getText(), "Test")){
+        System.out.println("Pass");
+    }
+}
     public static void main(String[] args) {
         new Main();
     }
+
 }
