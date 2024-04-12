c55b954d28e3a01e43632034086bca891982592c
Jacob Cortez <jacob.cortez@teamseco.com>
2024-04-12 18:55:27 -0400

    Create MyFrame class to create new Frames

---
+++ src/MyFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    MyFrame(){
+        this.setTitle("ACME");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(420,420);
+        this.setVisible(true);
+    }
+}
